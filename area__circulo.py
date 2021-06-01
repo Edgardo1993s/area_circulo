e03b84811bdb91d51795816ceecefe3f4d078283
selvin <selvinedgardo.com>
2021-05-31 20:57:44 -0600

    guardar programa

---
+++ area__circulo.py
@@ -0,0 +1,5 @@
+print("Programa para Calcular el area de un circulo")
+b=input("Ingrese la base del Circulo: ")
+altura=input("Ingrese la altura del circulo: ")
+area=int(b)*int(altura)/2
+print("El area del triangulo es: "+str(area))
